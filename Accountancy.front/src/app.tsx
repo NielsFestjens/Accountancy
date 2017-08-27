8427827cb947da8fc3428bb340e81f3e3c193544
Niels Festjens <niels.festjens@qframe.be>
2017-08-27 08:44:25 +0200

    basic routing

--- Accountancy.front/src/app.tsx
+++ Accountancy.front/src/app.tsx
@@ -1,8 +1,11 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import Hello from "./Hello";
+import BasicExample from "./BasicExample";
+
 
 ReactDOM.render(
-  <Hello name="WW" />,
+//   <Hello name="WW" />,
+  <BasicExample />,
   document.getElementById("root")
 );
